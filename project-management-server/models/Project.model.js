0027a45831b7139cf283411ad2c23386c6884528
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-04-10 19:19:49 +0200

    compleate project routes and post task route

---
+++ project-management-server/models/Project.model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const projectSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Project", projectSchema);
